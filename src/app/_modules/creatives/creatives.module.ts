d79ce013ee21b379b96edeb3bd2742209ba72549
Roman Himadinov <roman@brandetize.com>
2020-02-02 15:36:00 -0800

    updated creatives module

--- src/app/_modules/creatives/creatives.module.ts
+++ src/app/_modules/creatives/creatives.module.ts
@@ -1,12 +1,40 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { HttpClientModule } from '@angular/common/http';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreModule, ActionReducer } from '@ngrx/store';
+import { storeLogger } from 'ngrx-store-logger';
 
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { NgxPaginationModule } from 'ngx-pagination';
+
+import { CreativeComponent } from './components/creative/creative.component';
+import { CreativeListComponent } from './components/creative-list/creative-list.component';
+
+import { creativesReducer } from './common/store/creatives.reducers';
+import { CreativesEffects } from './common/store/creatives.effects';
+import { CreativesService } from './common/services/creatives.service';
+
+export function logger(reducer: ActionReducer<any>): any {
+  return storeLogger()(reducer);
+}
+
+export const metaReducers = [logger];
 
 @NgModule({
-  declarations: [],
+  declarations: [CreativeComponent, CreativeListComponent],
   imports: [
-    CommonModule
-  ]
+    CommonModule,
+    HttpClientModule,
+    NgbModule,
+    NgxPaginationModule,
+    StoreModule.forRoot({}),
+    StoreModule.forFeature('creatives', creativesReducer, { metaReducers }),
+    EffectsModule.forRoot([]),
+    EffectsModule.forFeature([CreativesEffects])
+  ],
+  exports: [CreativeListComponent, CreativeComponent],
+  providers: [CreativesService]
 })
-export class CreativesModule { }
+export class CreativesModule {}
