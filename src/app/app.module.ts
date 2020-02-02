6706f3349932e475b55e62ba64204d8a1aab180e
Roman Himadinov <roman@brandetize.com>
2020-02-02 14:08:04 -0800

    added modules: Campaigns and Creatives

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { CampaignsModule } from './_modules/campaigns/campaigns.module';
+import { CreativesModule } from './_modules/creatives/creatives.module';
+
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule, CampaignsModule, CreativesModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
