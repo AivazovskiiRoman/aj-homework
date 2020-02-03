03a2283360592ea04801deccf5155aba6ca673ba
Roman Himadinov <roman@brandetize.com>
2020-02-02 22:16:34 -0800

    added routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { CampaignListComponent } from './_modules/campaigns/components/campaign-list/campaign-list.component';
+import { CreativeListComponent } from './_modules/creatives/components/creative-list/creative-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: CampaignListComponent },
+  { path: 'creatives', component: CreativeListComponent },
+  { path: '**', redirectTo: '/' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
