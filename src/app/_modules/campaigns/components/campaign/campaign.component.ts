2e802389d4305cb885e3cff0a91e16dab48e21f3
Roman Himadinov <roman@brandetize.com>
2020-02-03 07:14:06 -0800

    campaign: added openCreatives func

--- src/app/_modules/campaigns/components/campaign/campaign.component.ts
+++ src/app/_modules/campaigns/components/campaign/campaign.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Store } from '@ngrx/store';
+import { Router } from '@angular/router';
 
 import { Campaign } from '../../common/models/campaign';
 import * as Campaigns from '../../common/store/campaigns.actions';
@@ -12,11 +13,15 @@ import * as Campaigns from '../../common/store/campaigns.actions';
 export class CampaignComponent implements OnInit {
   @Input() campaign: Campaign;
 
-  constructor(private store: Store<any>) {}
+  constructor(private store: Store<any>, private router: Router) {}
 
   ngOnInit() {}
 
   deleteCampaign(id: number) {
     this.store.dispatch(new Campaigns.DeleteCampaign(id));
+  }
+
+  openCreatives(id: number) {
+    this.router.navigate(['/creatives-of-campaign/' + id]);
   }
 }
